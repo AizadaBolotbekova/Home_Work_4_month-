395844e4056602d7b4117baaf0d9734ad068b3f2
AizadaBolotbekova <120017125+AizadaBolotbekova@users.noreply.github.com>
2023-02-25 10:56:09 +0300

    Add files via upload

---
+++ DZ3_Aizada_21_03/DZ3_Aizada_21_03/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TodoList from './page/TodoList/TodoList';
+
+// import Header from  './components/Header/Header';
+// import Button from './components/Button/Button';
+// import Footer from './components/footer/Footer';
+// import Text from './components/Text/Text';
+// import Block from './components/block/Block';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+export default App;
