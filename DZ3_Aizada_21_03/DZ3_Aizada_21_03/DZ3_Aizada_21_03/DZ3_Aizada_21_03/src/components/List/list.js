47bb79f690da56fda68df43ec79da4b5aa9d7acb
AizadaBolotbekova <120017125+AizadaBolotbekova@users.noreply.github.com>
2023-02-25 11:07:14 +0300

    Add files via upload

---
+++ DZ3_Aizada_21_03/DZ3_Aizada_21_03/DZ3_Aizada_21_03/DZ3_Aizada_21_03/src/components/List/list.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoCard from "../TodoCard/TodoCard";
+const List = ({ list, handleDone, handleDelete }) => {
+  return (
+    <>
+    {list.map((todo) => <TodoCard
+    key={todo.id}
+    todo={todo}
+    handleDone={handleDone}
+    handleDelete={handleDelete}
+    />)}
+    </>
+  )
+}
+export default List
